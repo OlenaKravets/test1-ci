01df4aa5cafda31238949d5f343de095d59a7f38
Home Macbook <homemacbook@MacBookAir.home>
2025-06-19 20:57:47 +0200

    🔁 Update tests to use jsonplaceholder public API

--- api/test_api_users.py
+++ api/test_api_users.py
@@ -1,16 +1,19 @@
 import requests
 
-BASE_URL = "https://reqres.in/api"
+BASE_URL = "https://jsonplaceholder.typicode.com"
 
 def test_get_users():
-    url = f"{BASE_URL}/users?page=1"
+    url = f"{BASE_URL}/users"
     response = requests.get(url)
-    assert response.status_code == 200
+    print("🔍 GET:", response.status_code, response.text)
+    assert response.status_code == 200, f"❌ Очікували 200, отримали {response.status_code}"
 
 def test_create_user():
     payload = {
         "name": "Olena",
-        "job": "QA Engineer"
+        "username": "qa_olena"
     }
     response = requests.post(f"{BASE_URL}/users", json=payload)
-    assert response.status_code == 201
+    print("📨 POST:", response.status_code, response.text)
+    assert response.status_code == 201 or response.status_code == 200, \
+        f"❌ Очікували 200 або 201, отримали {response.status_code}"
